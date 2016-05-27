9e9089e929abdb52691ac4f5295c20c9a6103524
adeyura <ade.yusuf.r@gmail.com>
2016-05-27 22:00:36 +0700

    add programSederhana
    add programSederhana

---
+++ programSederhana/views/site/about.php
@@ -0,0 +1,17 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'About';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        Web dengan fitur login yang diimplementasikan menggunakan database dan pengiriman pesan melalui menu kontak. <br>
+        Oleh Ade Yusuf Rahardian / 13514079
+    </p>
+</div>
